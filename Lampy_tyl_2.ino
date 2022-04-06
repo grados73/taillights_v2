334a1f2c426d5ee25984bf46331e7aceec560e51
grad73 <gradowski.kam@gmail.com>
2022-04-06 14:15:41 +0200

    A blank sketch of the second version of the back lamp has been added to perform the new state machine logic.
    Due to the possibility of code changes due to the tail lamp change and the need for new animations, the state machine logic will be updated and re-written to facilitate future development.

---
+++ Lampy_tyl_2.ino
@@ -0,0 +1,21 @@
+  /*
+  ******************************************************************************
+  ******************************************************************************
+  * @file       : Lampy_tyl_2
+  * @project		: Electric Car
+  * @author			: grados73 - https://github.com/grados73
+  * @purpose		: File with program to control rear lamp
+  * @TODO			  : New logic of state machine
+  ******************************************************************************
+  ******************************************************************************
+  */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
