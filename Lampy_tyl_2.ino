99361aecda229031547ab24f25603d3874a9df79
grad73 <gradowski.kam@gmail.com>
2022-04-07 21:00:42 +0200

    Added main function with switch-case to change state of Lamps after get a command.

--- Lampy_tyl_2.ino
+++ Lampy_tyl_2.ino
@@ -11,6 +11,9 @@
   */
 #include <Adafruit_NeoPixel.h>
 
+typedef int receivedCommand;
+receivedCommand NewCommand;
+
 //
 // Structure of LAMPS current state
 //
@@ -28,36 +31,39 @@ LAMP_STATE_TURN_L_HEADLIGHTS,   //9
 LAMP_STATE_TURN_R_HEADLIGHTS,   //10
 LAMP_STATE_REVERSE,             //11
 LAMP_STATE_STOP_REVERSE,        //12
-LAMP_STATE_HAZARD_REVERSE,      //13
+LAMP_STATE_HAZARD_STOP,         //13
 LAMP_STATE_LIGHTS_OFF,          //14
 LAMP_STATE_ACTIVATION,          //15     
 LAMP_STATE_SHUTDOWN             //16
 } RearLampState;
 
-// Routinse made in every state
-void HeadlightsRoutine(void);
-void StopRoutine(void);
-void TurnLStopRoutine(void);
-void TurnRStopRoutine(void);
-void TurnLReverseRoutine(void);
-void TurnRReverseRoutine(void);
-void HazardLightsRoutine(void);
-void HazardReverseRoutine(void);
-void TurnLHeadlightsRoutine(void);
-void TurnRHeadlightsRoutine(void);
-void ReverseRoutine(void);
-void StopReverseRoutine(void);
-void HazardReverseRoutine(void);
-void LightsOffRoutine(void);
-void ActivationsRoutine(void);
-void ShutdownRoutine(void);
+RearLampState lampState;
+
+// Prototype of function routine made in every state
+void HeadlightsRoutine(receivedCommand NewCommand);
+void StopRoutine(receivedCommand NewCommand);
+void TurnLStopRoutine(receivedCommand NewCommand);
+void TurnRStopRoutine(receivedCommand NewCommand);
+void TurnLReverseRoutine(receivedCommand NewCommand);
+void TurnRReverseRoutine(receivedCommand NewCommand);
+void HazardLightsRoutine(receivedCommand NewCommand);
+void HazardReverseRoutine(receivedCommand NewCommand);
+void TurnLHeadlightsRoutine(receivedCommand NewCommand);
+void TurnRHeadlightsRoutine(receivedCommand NewCommand);
+void ReverseRoutine(receivedCommand NewCommand);
+void StopReverseRoutine(receivedCommand NewCommand);
+void HazardStopRoutine(receivedCommand NewCommand);
+void LightsOffRoutine(receivedCommand NewCommand);
+void ActivationsRoutine(receivedCommand NewCommand);
+void ShutdownRoutine(receivedCommand NewCommand);
+
 
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   // initialize variable enum which will hold current state of rear lamp
-  RearLampState LampState = LAMP_STATE_ACTIVATION;
+  lampState = LAMP_STATE_ACTIVATION;
 }
 
 void loop() {
@@ -77,12 +83,72 @@ void loop() {
 
 void SwitchLampRegular()
 {
-  switch(LampState)
+  switch(lampState)
   {
-    case LAMP_STATE_ACTIVATION:
-      ActivationRoutine();
+    case LAMP_STATE_HEADLIGHTS:
+      HeadlightsRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_STOP:
+      StopRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_TURN_L_STOP:
+      TurnLStopRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_TURN_R_STOP:
+      TurnRStopRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_TURN_L_REVERSE:
+      TurnLReverseRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_TURN_R_REVERSE:
+      TurnRReverseRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_HAZARD_LIGHTS:
+      HazardLightsRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_HAZARD_REVERSE:
+      HazardReverseRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_TURN_L_HEADLIGHTS:
+      TurnLHeadlightsRoutine(NewCommand);
       break;
     
+    case LAMP_STATE_TURN_R_HEADLIGHTS:
+      TurnRHeadlightsRoutine(NewCommand);
+      break;
+    
+    case LAMP_STATE_REVERSE:
+      ReverseRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_STOP_REVERSE:
+      StopReverseRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_HAZARD_STOP:
+      HazardStopRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_LIGHTS_OFF:
+      LightsOffRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_ACTIVATION:
+      ActivationsRoutine(NewCommand);
+      break;
+
+    case LAMP_STATE_SHUTDOWN:
+      ShutdownRoutine(NewCommand);
+      break;
+      
     default:
       break;
   }
