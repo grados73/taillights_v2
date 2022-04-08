9094bc30f743e7113c974ae88b8380e415855866
grad73 <gradowski.kam@gmail.com>
2022-04-08 17:46:01 +0200

    Added possible commands, and initialize WS18b20.

--- Lampy_tyl_2.ino
+++ Lampy_tyl_2.ino
@@ -11,8 +11,29 @@
   */
 #include <Adafruit_NeoPixel.h>
 
-typedef int receivedCommand;
-receivedCommand NewCommand;
+
+//Control commands to receive
+#define STOP 1  
+#define HEADLIGHTS 5 
+#define REVERSE 7
+#define HAZARD 2 
+#define RIGHT_INDICATOR 9
+#define LEFT_INDICATOR 6
+#define ACTIVATION_SYSTEM 4
+#define SHUTDOWN_SYSTEM 14
+#define TURN_OFF_INDICATORS 12
+#define TURN_OFF_STOP 11
+#define TURN_OFF_REVRESE 17
+#define TURN_OF_HEADLIGHTS 15
+
+#define ledStripPin1t 12                  //Left strip connection Pin
+#define ledStripPin2t 11                  //Right strip connection Pin
+
+#define ledCount 10                       //Number of led in each strip
+#define Brightness 5                      //0 to 255
+#define lengthOfReverseLight 3            //How many Led shine white during reversing
+#define indicatorTimeDurationInMs 500     //How long indicators and hazard will blink
+
 
 //
 // Structure of LAMPS current state
@@ -37,7 +58,16 @@ LAMP_STATE_ACTIVATION,          //15
 LAMP_STATE_SHUTDOWN             //16
 } RearLampState;
 
+// Variable to hold current state of lamps
 RearLampState lampState;
+
+// Flag signalizing after get new command
+bool ChangeStateFlag = false;
+
+typedef int receivedCommand;
+receivedCommand NewCommand;
+
+String dataReceivedString = ""; //Empty string to hold received data
 
 // Prototype of function routine made in every state
 void HeadlightsRoutine(receivedCommand NewCommand);
@@ -57,6 +87,14 @@ void LightsOffRoutine(receivedCommand NewCommand);
 void ActivationsRoutine(receivedCommand NewCommand);
 void ShutdownRoutine(receivedCommand NewCommand);
 
+// Prototype of function to change string to int TODO!
+int StoI_f(String daneString);
+
+void clearLed12t();
+
+// Initialize of two strips with WS18B20
+Adafruit_NeoPixel strip1t(ledCount, ledStripPin1t, NEO_GRB + NEO_KHZ800); // rear left
+Adafruit_NeoPixel strip2t(ledCount, ledStripPin2t, NEO_GRB + NEO_KHZ800); // rear rignt
 
 
 void setup() {
@@ -64,23 +102,26 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   // initialize variable enum which will hold current state of rear lamp
   lampState = LAMP_STATE_ACTIVATION;
+
+  Serial.begin(115200); //uart initialization, SET SPEED = !!!115200!!!
+  strip1t.begin();
+  strip2t.begin();
+  strip1t.setBrightness(Brightness); //set brightness
+  strip2t.setBrightness(Brightness); //set brightness
+  clearLed12t();
 }
 
+//
+// Main program loop
+//
 void loop() {
-  
-  //
-  //Code to check if arduino work correctly
-  //
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                        // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100); 
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);  
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                         // wait for a second
+
 }
 
+
+//
+// Main function to change state of lamps after get new command
+//
 void SwitchLampRegular()
 {
   switch(lampState)
@@ -152,4 +193,125 @@ void SwitchLampRegular()
     default:
       break;
   }
+}
+
+
+
+void HeadlightsRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void StopRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void TurnLStopRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void TurnRStopRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void TurnLReverseRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void TurnRReverseRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void HazardLightsRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void HazardReverseRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void TurnLHeadlightsRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void TurnRHeadlightsRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void ReverseRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void StopReverseRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void HazardStopRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void LightsOffRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void ActivationsRoutine(receivedCommand NewCommand)
+{
+
+}
+
+void ShutdownRoutine(receivedCommand NewCommand)
+{
+
+}
+
+
+
+
+void clearLed12t()
+{
+  
+}
+
+
+
+
+
+
+
+int StoI_f(String daneString){
+  int odebraneDaneInt = 0;
+  if( daneString == "1") odebraneDaneInt = 1;
+  else if( daneString == "2") odebraneDaneInt = 2;
+  else if( daneString == "3") odebraneDaneInt = 3;
+  else if( daneString == "4") odebraneDaneInt = 4;
+  else if( daneString == "5") odebraneDaneInt = 5;
+  else if( daneString == "6") odebraneDaneInt = 6;
+  else if( daneString == "7") odebraneDaneInt = 7;
+  else if( daneString == "8") odebraneDaneInt = 8;
+  else if( daneString == "9") odebraneDaneInt = 9;
+  else if( daneString == "10") odebraneDaneInt = 10;
+  else if( daneString == "11") odebraneDaneInt = 11;
+  else if( daneString == "12") odebraneDaneInt = 12;
+  else if( daneString == "13") odebraneDaneInt = 13;
+  else if( daneString == "14") odebraneDaneInt = 14;
+  else if( daneString == "15") odebraneDaneInt = 15;
+  else if( daneString == "16") odebraneDaneInt = 16;
+  else if( daneString == "17") odebraneDaneInt = 17;
+  else if( daneString == "18") odebraneDaneInt = 18;
+  else if( daneString == "19") odebraneDaneInt = 19;
+  //else odebraneDaneInt = 0;
+  return odebraneDaneInt;
 }
